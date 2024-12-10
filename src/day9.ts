35e1a48d92200792a6c946a4c024bf7f97c3ed50
NuclearRedeye <github@mneah.com>
2024-12-10 14:07:52 +0000

    chore: cleanup day 9 comments

--- src/day9.ts
+++ src/day9.ts
@@ -136,12 +136,12 @@ answer = exercise1(real);
 console.log(`- Exercise 1 = '${answer}'`);
 console.assert(answer === 6201130364722);
 
-// // Exercise 2: Test Case
+// Exercise 2: Test Case
 answer = exercise2(test);
 console.log(`- Test 2 = '${answer}'`);
 console.assert(answer === 2858);
 
-// // // Exercise 2: Answer
+// Exercise 2: Answer
 answer = exercise2(real);
 console.log(`- Exercise 2 = '${answer}'`);
 console.assert(answer === 6221662795602);
